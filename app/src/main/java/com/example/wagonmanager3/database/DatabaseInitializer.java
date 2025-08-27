3d5022ef064218116b5909d792ba3ba3b9e5a88a
rorlov <info@dahushim.com>
2025-08-27 19:02:35 +0300

    - forgotten changes

--- app/src/main/java/com/example/wagonmanager3/database/DatabaseInitializer.java
+++ app/src/main/java/com/example/wagonmanager3/database/DatabaseInitializer.java
@@ -19,42 +19,42 @@ public class DatabaseInitializer {
         String wagon2Uuid = UUID.randomUUID().toString();
         String wagon3Uuid = UUID.randomUUID().toString();
         // Добавляем группы инвентаря
-//        InventoryGroup group1 = createGroup(UUID.randomUUID().toString(),"Внутреннее оборудование", wagon1Uuid, "Оборудование внутри вагона");
-//        InventoryGroup group11 = createGroup(UUID.randomUUID().toString(),"Внутреннее оборудование", wagon2Uuid, "Оборудование внутри вагона");
-//        InventoryGroup group12 = createGroup(UUID.randomUUID().toString(),"Внутреннее оборудование", wagon3Uuid, "Оборудование внутри вагона");
-//
-//        InventoryGroup group2 = createGroup(UUID.randomUUID().toString(),"Электрооборудование", wagon1Uuid, "Электрические компоненты");
-//        InventoryGroup group21 = createGroup(UUID.randomUUID().toString(),"Электрооборудование", wagon2Uuid, "Электрические компоненты");
-//        InventoryGroup group22 = createGroup(UUID.randomUUID().toString(),"Электрооборудование", wagon3Uuid, "Электрические компоненты");
-//
-//        InventoryGroup group3 = createGroup(UUID.randomUUID().toString(),"Сантехника", wagon1Uuid, "Сантехническое оборудование");
-//        InventoryGroup group31 = createGroup(UUID.randomUUID().toString(),"Сантехника", wagon2Uuid, "Сантехническое оборудование");
-//        InventoryGroup group32 = createGroup(UUID.randomUUID().toString(),"Сантехника", wagon3Uuid, "Сантехническое оборудование");
-//
-//        // Добавляем инвентарь для групп
-//        createInternalEquipmentItems(group1.getUuid(), wagon1Uuid);
-//        createElectricalEquipmentItems(group2.getUuid(), wagon2Uuid);
-//        createPlumbingEquipmentItems(group3.getUuid(), wagon3Uuid);
-//
-//        createPlumbingEquipmentItems(group11.getUuid(), wagon2Uuid);
-//        createPlumbingEquipmentItems(group12.getUuid(), wagon3Uuid);
-//        createPlumbingEquipmentItems(group21.getUuid(), wagon2Uuid);
-//        createPlumbingEquipmentItems(group22.getUuid(), wagon3Uuid);
-//        createPlumbingEquipmentItems(group31.getUuid(), wagon2Uuid);
-//        createPlumbingEquipmentItems(group32.getUuid(), wagon3Uuid);
-//
-//
-//        User responsibleUser = createUser("responsible", "Ответственный", "responsible");
-//        User conductorUser = createUser("conductor", "Проводник Иванов", "conductor");
-//
-//        Wagon wagon1 = createWagon("Вагон 1321", wagon1Uuid, "Пассажирский");
-//        Wagon wagon2 = createWagon("Вагон 2495", wagon2Uuid, "Пассажирский");
-//        Wagon wagon3 = createWagon("Вагон 3753", wagon3Uuid, "Грузовой");
-//
-//        // Добавляем по одному сканированию для каждого вагона
-//        createSingleScanHistory(wagon1, conductorUser);
-//        createSingleScanHistory(wagon2, conductorUser);
-//        createSingleScanHistory(wagon3, responsibleUser);
+        InventoryGroup group1 = createGroup(UUID.randomUUID().toString(),"Внутреннее оборудование", wagon1Uuid, "Оборудование внутри вагона");
+        InventoryGroup group11 = createGroup(UUID.randomUUID().toString(),"Внутреннее оборудование", wagon2Uuid, "Оборудование внутри вагона");
+        InventoryGroup group12 = createGroup(UUID.randomUUID().toString(),"Внутреннее оборудование", wagon3Uuid, "Оборудование внутри вагона");
+
+        InventoryGroup group2 = createGroup(UUID.randomUUID().toString(),"Электрооборудование", wagon1Uuid, "Электрические компоненты");
+        InventoryGroup group21 = createGroup(UUID.randomUUID().toString(),"Электрооборудование", wagon2Uuid, "Электрические компоненты");
+        InventoryGroup group22 = createGroup(UUID.randomUUID().toString(),"Электрооборудование", wagon3Uuid, "Электрические компоненты");
+
+        InventoryGroup group3 = createGroup(UUID.randomUUID().toString(),"Сантехника", wagon1Uuid, "Сантехническое оборудование");
+        InventoryGroup group31 = createGroup(UUID.randomUUID().toString(),"Сантехника", wagon2Uuid, "Сантехническое оборудование");
+        InventoryGroup group32 = createGroup(UUID.randomUUID().toString(),"Сантехника", wagon3Uuid, "Сантехническое оборудование");
+
+        // Добавляем инвентарь для групп
+        createInternalEquipmentItems(group1.getUuid(), wagon1Uuid);
+        createElectricalEquipmentItems(group2.getUuid(), wagon2Uuid);
+        createPlumbingEquipmentItems(group3.getUuid(), wagon3Uuid);
+
+        createPlumbingEquipmentItems(group11.getUuid(), wagon2Uuid);
+        createPlumbingEquipmentItems(group12.getUuid(), wagon3Uuid);
+        createPlumbingEquipmentItems(group21.getUuid(), wagon2Uuid);
+        createPlumbingEquipmentItems(group22.getUuid(), wagon3Uuid);
+        createPlumbingEquipmentItems(group31.getUuid(), wagon2Uuid);
+        createPlumbingEquipmentItems(group32.getUuid(), wagon3Uuid);
+
+
+        User responsibleUser = createUser("responsible", "Ответственный", "responsible");
+        User conductorUser = createUser("conductor", "Проводник Иванов", "conductor");
+
+        Wagon wagon1 = createWagon("Вагон 1321", wagon1Uuid, "Пассажирский");
+        Wagon wagon2 = createWagon("Вагон 2495", wagon2Uuid, "Пассажирский");
+        Wagon wagon3 = createWagon("Вагон 3753", wagon3Uuid, "Грузовой");
+
+        // Добавляем по одному сканированию для каждого вагона
+        createSingleScanHistory(wagon1, conductorUser);
+        createSingleScanHistory(wagon2, conductorUser);
+        createSingleScanHistory(wagon3, responsibleUser);
     }
 
     private void createSingleScanHistory(Wagon wagon, User user) {
